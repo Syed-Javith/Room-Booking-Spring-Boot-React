cf3ea018ed98b975ad7fbdf5f85c0605adfdb9c6
Syed-Javith <syedjavith14@gmail.com>
2024-07-28 16:39:17 +0530

    Utility class added

--- server/src/main/java/com/hotel_booking/server/Utils/Utils.java
+++ server/src/main/java/com/hotel_booking/server/Utils/Utils.java
@@ -1,2 +1,7 @@
-package com.hotel_booking.server.Utils;public class Utils {
+package com.hotel_booking.server.Utils;
+
+public class Utils {
+    public static String replaceUserName(String body,String userName) {
+        return body.replace("[Guest Name]",userName);
+    }
 }
